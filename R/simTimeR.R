674bd7721c75b927e9cb5c213b2c49319b6ca61b
Christoph Strauss <christoph.strauss@fhsg.ch>
2016-01-04 17:16:44 +0100

    Add Warnings when POSIXct commands exist. Changes in documentation Make return values explicit

--- R/simTimeR.R
+++ R/simTimeR.R
@@ -1,35 +1,31 @@
-#' Converts a DateTime to a simTime. truncates the Date part. range 0-(24*60*60-1)
-#'
+# =============================================================================
+# Independent functions:
+
+#' Converts a DateTime to a simTime. Only time part of datetime. range 0-(24*60*60-1).
 #' @param datetime
 #'
 #' @return t A simDateTime
 as.simTime <- function(datetime) {
   datetime <- as.character(datetime)
-  60*60*as.integer(strftime(datetime, "%H")) +
+  seconds <- 60*60*as.integer(strftime(datetime, "%H")) +
     60*as.integer(strftime(datetime, "%M")) +
     as.integer(strftime(datetime, "%S"))
+  return(seconds)
 }
 
 
-#' Converts a DateTime to a simDate. truncates the Time part. range 0-364
+#' Converts a DateTime to a simDate. Only date part of datetime. range 0-364.
 #'
 #' @param datetime
 #'
 #' @return t A simDate
 as.simDate <- function(datetime) {
+  warning("Implementation via POSIXct possible. See testthat")
   datetime <- as.character(datetime)
-  as.integer(strftime(datetime, "%j")) - 1
+  days <- as.integer(strftime(datetime, "%j")) - 1 # Day of year. 1-365.
+  return(days)
 }
 
-
-#' Converts a DateTime to a simDateTime
-#'
-#' @param datetime
-#'
-#' @return t A simDateTime
-as.simDateTime <- function(datetime) {
-  as.simDate(datetime)*24*60*60 + as.simTime(datetime)
-}
 
 #' as.charTime(): Converts a simTime back to a char containing the time in the
 #' format HH:MM
@@ -38,21 +34,25 @@ as.simDateTime <- function(datetime) {
 #'
 #' @return charTime A char containing a time in the format HH:MM
 as.charTime <- function(simTime) {
+  warning("Implementation via POSIXct possible. See testthat")
   h <- trunc(simTime/60/60)
   if(h < 10) {h <- as.character(paste0(0,h))}
   m <- simTime %% (60*60)/60
   if(m < 10) {m <- as.character(paste0(0,m))}
-  paste0(h,":",m)
+  return(paste0(h,":",m))
 }
 
-#' Date part of a simDateTime
+
+#' Return day of year from rel. time in sconds.
 #'
 #' @param t A simDateTime
 #'
 #' @return the day of year (0-364)
 simDate <- function(t) {
-  trunc(t/(24*60*60))
+  warning("Implementation via POSIXct possible. See testthat. ToDo vals 0, 365?")
+  return(trunc(t/(24*60*60)))
 }
+
 
 #' Time part of a simDateTime
 #'
@@ -60,7 +60,7 @@ simDate <- function(t) {
 #'
 #' @return the actual time in seconds (0-(24*60*60-1))
 simTime <- function(t) {
-  t %% (24*60*60)
+  return(t %% (24*60*60))
 }
 
 
@@ -70,5 +70,26 @@ simTime <- function(t) {
 #'
 #' @return the weekday in the format: "Mo, Di, Mi, Do, Fr, Sa, So"
 simWeekday <- function(t) {
+
   rep(c("Mi", "Do", "Fr", "Sa", "So", "Mo", "Di"), length = 365)[simDate(t) + 1]
 }
+
+
+# =============================================================================
+# Functions with dependencies:
+
+#' Converts a DateTime to a simDateTime. Date + Time in seconds. Range 0- (365*24*60*60-1)
+#'
+#' @param datetime
+#'
+#' @return t A simDateTime
+as.simDateTime <- function(datetime) {
+  warning("Implementation via POSIXct possible. See testthat")
+  seconds <- as.simDate(datetime)*24*60*60 + as.simTime(datetime)
+  return(seconds)
+}
+
+
+# =============================================================================
+# Not used any more:
+
