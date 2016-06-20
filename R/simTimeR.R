8737a92ec15bdc273325a4dcd271d8329b61d78e
Christoph Strauss <christoph.strauss@fhsg.ch>
2016-06-20 11:14:25 +0200

    Original version. No changes.

--- R/simTimeR.R
+++ R/simTimeR.R
@@ -70,14 +70,12 @@ simTime <- function(t) {
 #'
 #' @return the weekday in the format: "Mo, Di, Mi, Do, Fr, Sa, So"
 simWeekday <- function(t, order = c("Mi", "Do", "Fr", "Sa", "So", "Mo", "Di")) {
-  # rep(order, length = 365)[simDate(t) + 1]
+  rep(order, length = 365)[simDate(t) + 1]
   # Test alternative:
-  if (exists("n_sim_weekday", env = .GlobalEnv, inherits = F)) {
-    print(n_sim_weekday)
-    assign("n_sim_weekday", n_sim_weekday + 1, envir = .GlobalEnv)
-  }
-  weekdays(as.POSIXct(t, tz = "GMT", origin = "2014-01-01 00:00:00"), abbreviate = T) # german "Do" "Fr" "Sa" "So" "Mo" "Di" "Mi"
-  # lubridate::wday(as.POSIXct(t_int[1], tz = "GMT", origin = "1970-01-01"), label = T, abbr = T) # english Thurs Fri   Sat   Sun   Mon   Tues  Wed
+  # german "Do" "Fr" "Sa" "So" "Mo" "Di" "Mi". Change using
+  # Sys.setlocale(category = "LC_ALL", locale = "English_United States.1252")
+  # weekdays(as.POSIXct(t, tz = "GMT", origin = "2014-01-01 00:00:00"), abbreviate = T)
+  # imsbasics::e2g(as.character(lubridate::wday(as.POSIXct(t, tz = "GMT", origin = "2014-01-01 00:00:00"), label = T, abbr = T))) # english Thurs Fri   Sat   Sun   Mon   Tues  Wed
 }
 
 
