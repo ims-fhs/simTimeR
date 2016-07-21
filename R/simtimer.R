92b7fb1780a9ee296ac0169301b826bce62c3be6
Christoph Strauss <christoph.strauss@fhsg.ch>
2016-07-21 14:39:53 +0200

    Rename project to simtimer. All "old" simtimer tests are running again. Idea: Split schedule off.

--- R/simtimer.R
+++ R/simtimer.R
@@ -1,108 +1,65 @@
-# Old simtimer functions without changes for backwards compatibility (except
-# package name) - DO NOT MAKE CHANGES HERE!!!
+# simtimer converts date-times to a relative timescale based on seconds.
+# The functions are faster than any other implementation using dates.
+# * datetime functions and variables always refers to date-times
+# * sim_ functions and variables always refers to sim_"times"
+#
+# origin_date needs day-time 00:00:00. Otherwise simtimer leads to wrong results.
+# tz will be ignored at the moment.
+# datetime is always POSIXct date-time.
 
-#' Converts a DateTime to a simTime. Only time part of datetime. range 0-(24*60*60-1).
-#' @param datetime
+#' Return day of year from rel. time in seconds.
 #'
-#' @return t A simDateTime
+#' @param sim_datetime A sim_datetime
+#'
+#' @return the day of the period (> 0, in many cases also < 365)
 #' @export
-as.simTime <- function(datetime) {
-  datetime <- as.character(datetime)
-  seconds <- 60*60*as.integer(strftime(datetime, "%H")) +
-    60*as.integer(strftime(datetime, "%M")) +
-    as.integer(strftime(datetime, "%S"))
-  return(seconds)
+sim_date <- function(sim_datetime) {
+  return(trunc(sim_datetime/(24*60*60)))
 }
 
-
-#' Converts a DateTime to a simDate. Only date part of datetime. range 0-364.
+#' Day-time part of a sim_datetime
 #'
-#' @param datetime
+#' @param sim_datetime A sim_datetime
 #'
-#' @return t A simDate
+#' @return the actual day-time in seconds (0-(24*60*60-1))
 #' @export
-as.simDate <- function(datetime) {
-  # warning("Implementation via POSIXct possible. See testthat")
-  datetime <- as.character(datetime)
-  days <- as.integer(strftime(datetime, "%j")) - 1 # Day of year. 1-365.
-  return(days)
+sim_time <- function(sim_datetime) {
+  return(sim_datetime %% (24*60*60))
 }
 
-
-#' as.charTime(): Converts a simTime back to a char containing the time in the
-#' format HH:MM
+#' Weekday of a sim_datetime
 #'
-#' @param simTime
-#'
-#' @return charTime A char containing a time in the format HH:MM
-#' @export
-as.charTime <- function(simTime) { # ........................................... SQC zero_n
-  # warning("Implementation via POSIXct possible. See testthat")
-  h <- trunc(simTime/60/60)
-  if(h < 10) {h <- as.character(paste0(0,h))}
-  m <- simTime %% (60*60)/60
-  if(m < 10) {m <- as.character(paste0(0,m))}
-  return(paste0(h,":",m))
-}
-
-
-#' Return day of year from rel. time in sconds.
-#'
-#' @param t A simDateTime
-#'
-#' @return the day of year (0-364)
-#' @export
-simDate <- function(t) {
-  # warning("Implementation via POSIXct possible. See testthat. ToDo vals 0, 365?")
-  return(trunc(t/(24*60*60)))
-}
-
-
-#' Time part of a simDateTime
-#'
-#' @param t A simDateTime
-#'
-#' @return the actual time in seconds (0-(24*60*60-1))
-#' @export
-simTime <- function(t) {
-  return(t %% (24*60*60)) # ..................... t_h <- strptime(t, "%Y-%m-%d") => t-t_h
-}
-
-
-#' Weekday of a simDateTime
-#'
-#' @param t A simDateTime
+#' @param sim_datetime A sim_datetime
 #'
 #' @return the weekday in the format: "Mo, Di, Mi, Do, Fr, Sa, So"
 #' @export
-simWeekday <- function(t, order = c("Mi", "Do", "Fr", "Sa", "So", "Mo", "Di")) {
-  return(rep(order, length = 365)[simDate(t) + 1])
+sim_wday <- function(sim_datetime) {
+  # tz = "GMT" not considered at the moment. In case of performance problems: Memoize.
+  wday <- lubridate::wday(as.POSIXct(sim_datetime, origin = origin_date),
+                          label = T, abbr = T)
+  return(as.character(wday))
 }
 
-#' Converts a DateTime to a simDateTime. Date + Time in seconds. Range 0- (365*24*60*60-1)
+#' Converts a datetime to a sim_datetime.
 #'
-#' @param datetime
+#' @param datetime A datetime
 #'
-#' @return t A simDateTime
+#' @return sim_datetime A sim_datetime
 #' @export
-as.simDateTime <- function(datetime) {
-  # warning("Implementation via POSIXct possible. See testthat")
-  seconds <- as.simDate(datetime)*24*60*60 + as.simTime(datetime)
+sim_datetime <- function(datetime) {
+  seconds <- as.integer(as.numeric(datetime - origin_date, units="secs"))
+  # seconds <- sim_date(datetime)*24*60*60 + sim_time(datetime)
   return(seconds)
 }
 
-#' is_on_duty from sim911 package finds all vehicles that are on duty
+#' Converts a sim_datetime to a datetime.
 #'
-#' @param vehicles A set of class sim_vehicle
-#' @param t A numeric, relative time in seconds
-#' @import data.table
+#' @param sim_datetime
 #'
-#' @return vehicles class sim_vehicles, a subset of sim_vehicles
+#' @return datetime A POSIXct date-time
 #' @export
-is_on_duty <- function(t, vehicles) { # ........................................ rule?
-  return(vehicles$shift_from_simdate <= simTimeR::simDate(t) &
-      vehicles$shift_to_simdate >= simTimeR::simDate(t) &
-      vehicles$shift_from_simtime <= simTimeR::simTime(t) &
-      vehicles$shift_to_simtime > simTimeR::simTime(t) &
-      grepl(simTimeR::simWeekday(t), vehicles$shift_weekday))
+datetime <- function(sim_datetime) {
+  datetime <- origin_date + sim_datetime
+  return(datetime)
 }
+
